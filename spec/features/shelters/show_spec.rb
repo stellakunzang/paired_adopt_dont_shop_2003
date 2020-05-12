c3912cab8370d71107b6512fd79a97b899b16aee
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-12 14:40:33 -0600

    Add test for see list of shelter reviews

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -31,7 +31,29 @@ RSpec.describe "shelter show page" do
     visit "/shelters/#{shelter_1.id}"
 
     click_link "View Pets"
-  
+
     expect(current_path).to eq("/shelters/#{shelter_1.id}/pets")
+  end
+
+  it "can see a list of reviews for that shelter" do
+    shelter_1 = Shelter.create(name: "Happy Puppies", address: "55 Street St", city: "Danger Mountain", state: "UT", zip: "80304")
+    review_1 = ShelterReview.create(title: "What a great place!", rating: "5", content: "We got our new puppy from Happy Puppies and they totally lived up to their name.", image: "image.jpeg")
+    review_2 = ShelterReview.create(title: "Puppy Time!", rating: "4", content: "Happy place, but could be cleaner.")
+    visit "/shelters/#{shelter_1.id}"
+
+    within("#shelter_review-#{review_1.id}") do
+      expect(page).to have_content(review_1.title)
+      expect(page).to have_content(review_1.rating)
+      expect(page).to have_content(review_1.content)
+      expect(page).to have_content(review_1.image)
+    end
+
+    within("#shelter_review-#{review_2.id}") do
+      expect(page).to have_content(review_2.title)
+      expect(page).to have_content(review_2.rating)
+      expect(page).to have_content(review_2.content)
+      expect(page).to have_no_content(review_2.image)
+    end
+
   end
 end
