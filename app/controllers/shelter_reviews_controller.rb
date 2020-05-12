f64591289cea90b058be47b0a9e7aa814d0f6c52
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-12 15:50:53 -0600

    Add create method for shelter reviews

--- app/controllers/shelter_reviews_controller.rb
+++ app/controllers/shelter_reviews_controller.rb
@@ -2,5 +2,15 @@ class ShelterReviewsController < ApplicationController
   def new
     @shelter_id = params[:shelter_id]
   end
-  
+
+  def create
+    ShelterReview.create(shelter_review_params)
+    redirect_to("/shelters#{params[:shelter_id]}")
+  end
+
+  private
+
+  def shelter_review_params
+    params.permit(:title, :rating, :content, :image, :shelter_id)
+  end
 end
