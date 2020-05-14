56f6dc241bb090595a92b11ec8ea1eac30bce7d2
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-14 07:11:17 -0600

    Add test for review edit page flash message

--- spec/features/shelter_reviews/edit_spec.rb
+++ spec/features/shelter_reviews/edit_spec.rb
@@ -23,4 +23,20 @@ RSpec.describe "shelter review edit page" do
     expect(page).to have_content("Cool dudes")
     expect(page).to have_no_content("What a great place!")
   end
+
+  it "can send a flash message if fields on edit page are left empty" do
+    shelter_1 = Shelter.create(name: "Happy Puppies", address: "55 Street St", city: "Danger Mountain", state: "UT", zip: "80304")
+
+    review_1 = ShelterReview.create!(title: "What a great place!", rating: "5", content: "We got our new puppy from Happy Puppies and they totally lived up to their name.", image: "image.jpeg", shelter_id: shelter_1.id)
+
+    visit "shelters/#{shelter_1.id}/reviews/#{review_1.id}/edit"
+
+    fill_in :title, with: ""
+
+    click_on "Update Review"
+
+    expect(current_path).to eq("shelters/#{shelter_1.id}/reviews/#{review_1.id}/edit")
+
+    expect(page).to have_content("In order to edit a review, you must enter a title, rating, and description.")
+  end
 end
