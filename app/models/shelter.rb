67c42050f12f48eb61299983a0b171bba968eddb
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-15 17:49:50 -0600

    Move validation lines in models to single line

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,9 +1,5 @@
 class Shelter < ApplicationRecord
   has_many :pets
   has_many :shelter_reviews
-  validates_presence_of :name
-  validates_presence_of :address
-  validates_presence_of :city
-  validates_presence_of :state
-  validates_presence_of :zip
+  validates_presence_of :name, :address, :city, :state, :zip 
 end
--- app/models/shelter_review.rb
+++ app/models/shelter_review.rb
@@ -1,6 +1,4 @@
 class ShelterReview < ApplicationRecord
   belongs_to :shelter
-  validates_presence_of :title
-  validates_presence_of :rating
-  validates_presence_of :content
+  validates_presence_of :title, :rating, :content 
 end
