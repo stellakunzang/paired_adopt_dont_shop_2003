680187e6d0975a370859bf5dcbc8e427a34d9c81
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-16 21:42:48 -0600

    Add flash message if fields are left blank on application

--- app/controllers/adoption_application_controller.rb
+++ app/controllers/adoption_application_controller.rb
@@ -5,10 +5,15 @@ class AdoptionApplicationController < ApplicationController
   end
 
   def create
-    new_application = AdoptionApplication.create(application_params)
+    new_application = AdoptionApplication.new(application_params)
     # :pets will be the Pet objects that are selected during the application
-    # will I be able to do this directly or will have to use the name to grab the pet objects and then associate them with :pet 
-    redirect_to "/favorites"
+    # will I be able to do this directly or will have to use the name to grab the pet objects and then associate them with :pet
+    if new_application.save
+      redirect_to "/favorites"
+    else
+      flash[:notice] = "All fields must be completed in order to submit an application."
+      redirect_to("/adoption_application/new")
+    end
   end
 
   private
