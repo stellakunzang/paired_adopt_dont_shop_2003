8934cd8a265db693e677818c99e5dedddd5148d8
Ana Perez Santos <a.jperez@yahoo.com>
2020-05-16 21:47:03 -0600

    US-10: Define index action / update update action to store data in sessions

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,15 @@
 class FavoritesController < ApplicationController
+  include ActionView::Helpers::TextHelper
+
+  def index
+    favorites = favorite.contents
+  end
 
   def update
     pet = Pet.find(params[:pet_id])
-    favorites.add_pet(pet.id)
-    session[:favorites] = favorites.contents
+    favorite.add_pet(pet.id)
+    session[:favorite] = favorite.contents
+    quantity = favorite.count_of(pet.id)
     flash[:notice] = "Pet has been added to your favorites list"
     redirect_to "/pets/#{pet.id}"
   end
