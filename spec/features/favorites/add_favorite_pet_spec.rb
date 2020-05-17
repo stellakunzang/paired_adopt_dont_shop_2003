9f1e642d20235f32bf92b9b623b53bb1bdd459af
Ana Perez Santos <a.jperez@yahoo.com>
2020-05-17 01:05:32 -0600

    US-12: Include test to differentiate between adding and removing pet from favorites

--- spec/features/favorites/add_favorite_pet_spec.rb
+++ spec/features/favorites/add_favorite_pet_spec.rb
@@ -25,5 +25,26 @@ RSpec.describe "Add Favorite Pet" do
       expect(page).to have_content("Pet has been added to your favorites list")
       expect(page).to have_content("Favorite Pets: 2")
     end
+
+    it "I cannot add a pet more than once" do
+      shelter = Shelter.create(name: "Happy Puppies", address: "55 Street St", city: "Danger Mountain", state: "UT", zip: "80304")
+      pet_1 = Pet.create(image: "image.jpeg", name: "Kunga", approximate_age: "1", sex: "male", shelter_id: shelter.id)
+
+      visit "/pets/#{pet_1.id}"
+
+      click_button("Add to Favorites")
+
+      expect(page).to have_content("Favorite Pets: 1")
+      expect(page).to_not have_button("Add to Favorites")
+      expect(page).to have_button("Remove from Favorites")
+
+      click_link("Remove from Favorites")
+
+      expect(current_path).to eq("/pets/#{pet_1.id}")
+      expect(page).to have_content("Pet has been removed from your favorites list")
+      expect(page).to_not have_button("Add to Favorites")
+
+      expect(page).to have_content("Favorite Pets: 0")
+    end
   end
 end
