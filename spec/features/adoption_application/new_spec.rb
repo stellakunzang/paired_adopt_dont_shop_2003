ff2452f9582e6322b1d911da730e48b7200ea9a6
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-16 20:32:53 -0600

    Add test for flash message when application submitted incomplete

--- spec/features/adoption_application/new_spec.rb
+++ spec/features/adoption_application/new_spec.rb
@@ -3,7 +3,14 @@ require "rails_helper"
 RSpec.describe "New Adoption Application" do
   it "can select pets from favorites to submit application to adopt" do
 
+    shelter = Shelter.create(name: "Happy Puppies", address: "55 Street St", city: "Danger Mountain", state: "UT", zip: "80304")
+    pet_1 = Pet.create(image: "image.jpeg", name: "Kunga", approximate_age: "1", sex: "male", shelter_id: shelter.id)
+
+    favorites.add_pet(pet_1.id)
+
     visit "/adoption_application/new"
+
+    select "Kunga", from: :favorites
 
     fill_in :name, with: "Stella"
     fill_in :address, with: "3300 Josephine St"
@@ -13,7 +20,32 @@ RSpec.describe "New Adoption Application" do
     fill_in :phone_number, with: "757-489-4936"
     fill_in :description, with: "I love animals more than people."
 
-    # click_on "Submit Application"
+    click_on "Submit Application"
+  end
+
+  xit "can display flash message if application fields are left blank" do
+
+    shelter = Shelter.create(name: "Happy Puppies", address: "55 Street St", city: "Danger Mountain", state: "UT", zip: "80304")
+    pet_1 = Pet.create(image: "image.jpeg", name: "Kunga", approximate_age: "1", sex: "male", shelter_id: shelter.id)
+
+    favorites.add_pet(pet_1.id)
+
+    visit "/adoption_application/new"
+
+    select "Kunga", from: :favorites
+
+    fill_in :name, with: "Stella"
+    fill_in :address, with: "3300 Josephine St"
+    fill_in :city, with: "Denver"
+    fill_in :state, with: "CO"
+    fill_in :zip, with: "80205"
+    fill_in :phone_number, with: ""
+    fill_in :description, with: ""
+
+    click_on "Submit Application"
+
+    expect(current_path).to eq("/adoption_application/new")
+    expect(page).to have_content("All fields must be completed in order to submit an application.")
   end
 
   describe "favorites index page" do
@@ -23,14 +55,28 @@ RSpec.describe "New Adoption Application" do
       pet_1 = Pet.create(image: "image.jpeg", name: "Kunga", approximate_age: "1", sex: "male", shelter_id: shelter.id)
       pet_2 = Pet.create(image: "image.jpeg", name: "Honey Pie", approximate_age: "11", sex: "female", shelter_id: shelter.id)
 
-      # add both pets to favorites
-      # click link on favorites index page to apply
-      # fill out and submit application
+      favorites.add_pet(pet_1.id)
+      favorites.add_pet(pet_2.id)
+
+      visit "/adoption_application/new"
+
+      select "Kunga", from: :favorites
+
+      fill_in :name, with: "Stella"
+      fill_in :address, with: "3300 Josephine St"
+      fill_in :city, with: "Denver"
+      fill_in :state, with: "CO"
+      fill_in :zip, with: "80205"
+      fill_in :phone_number, with: "757-489-4936"
+      fill_in :description, with: "I love animals more than people."
+
+      click_on "Submit Application"
 
       except(current_path).to eq("/favorites")
       expect(page).to have_content("Your application has been submitted!")
       expect(page).to have_no_content(pet_1.name)
-      expect(page).to have_no_content(pet_2.name)
+      expect(page).to have_content(pet_2.name)
     end
   end
+
 end
