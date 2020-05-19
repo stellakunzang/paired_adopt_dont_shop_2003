4cb5bb61a8f50451374481d5cddb8f626a71c77f
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-18 18:28:14 -0600

    Add test for favorites updating once pet has been applied for

--- spec/features/favorites/index_spec.rb
+++ spec/features/favorites/index_spec.rb
@@ -107,4 +107,36 @@ RSpec.describe "Favorite Pets Index", type: :feature do
 
     expect(current_path).to eq("/adoption_applications/new")
   end
+
+  it "can display flash message and update favorites once application is submitted" do
+
+    shelter = Shelter.create(name: "Happy Puppies", address: "55 Street St", city: "Danger Mountain", state: "UT", zip: "80304")
+    pet_1 = Pet.create(image: "image.jpeg", name: "Kunga", approximate_age: "1", sex: "male", shelter_id: shelter.id)
+    pet_2 = Pet.create(image: "image.jpeg", name: "Honey Pie", approximate_age: "11", sex: "female", shelter_id: shelter.id)
+
+    visit "/pets/#{pet_1.id}"
+    click_button("Add to Favorites")
+
+    visit "/pets/#{pet_2.id}"
+    click_button("Add to Favorites")
+
+    visit "/adoption_applications/new"
+
+    find("input[type='checkbox'][value='#{pet_1.id}']").set(true)
+
+    fill_in :name, with: "Stella"
+    fill_in :address, with: "3300 Josephine St"
+    fill_in :city, with: "Denver"
+    fill_in :state, with: "CO"
+    fill_in :zip, with: "80205"
+    fill_in :phone_number, with: "757-489-4936"
+    fill_in :description, with: "I love animals more than people."
+
+    click_on "Submit Application"
+
+    expect(current_path).to eq("/favorites")
+    expect(page).to have_content("Your application has been submitted!")
+    expect(page).to have_no_content(pet_1.name)
+    expect(page).to have_content(pet_2.name)
+  end
 end
