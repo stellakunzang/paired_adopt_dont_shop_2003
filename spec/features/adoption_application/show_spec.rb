01b492fafa33ac5f8f18f8cd2c1133167c9d229d
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-19 12:55:37 -0600

    Add test to not find approve application button once an application has been approved for specific pet

--- spec/features/adoption_application/show_spec.rb
+++ spec/features/adoption_application/show_spec.rb
@@ -46,4 +46,60 @@ RSpec.describe "applications show page" do
     expect(page).to have_content("adoption pending")
     expect(page).to have_content("This pet is on hold for Stella.")
   end
+
+  it "can approve application for multiple pets" do
+    shelter = Shelter.create(name: "Happy Puppies", address: "55 Street St", city: "Danger Mountain", state: "UT", zip: "80304")
+
+    pet_1 = Pet.create(image: "image.jpeg", name: "Kunga", approximate_age: "1", sex: "male", shelter_id: shelter.id)
+    pet_2 = Pet.create(image: "image.jpeg", name: "Honey Pie", approximate_age: "11", sex: "female", shelter_id: shelter.id)
+
+    application = AdoptionApplication.create(name: "Stella", address: "street", city: "City", state: "ST", zip: "34567", phone_number: "545-567-7643", description: "I'm awesome")
+
+    PetApplication.create(pet_id: pet_1.id, adoption_application_id: application.id)
+    PetApplication.create(pet_id: pet_2.id, adoption_application_id: application.id)
+
+    visit "/adoption_applications/#{application.id}"
+
+    within ".pets-#{pet_1.id}" do
+      click_link "Approve Application"
+    end
+
+    expect(current_path).to eq("/pets/#{pet_1.id}")
+    expect(page).to have_content("adoption pending")
+    expect(page).to have_content("This pet is on hold for Stella.")
+
+    visit "/adoption_applications/#{application.id}"
+
+    within ".pets-#{pet_2.id}" do
+      click_link "Approve Application"
+    end
+
+    expect(current_path).to eq("/pets/#{pet_2.id}")
+    expect(page).to have_content("adoption pending")
+    expect(page).to have_content("This pet is on hold for Stella.")
+  end
+
+  it "approve application link can be removed once application for specific pet has been approved" do
+    shelter = Shelter.create(name: "Happy Puppies", address: "55 Street St", city: "Danger Mountain", state: "UT", zip: "80304")
+
+    pet_1 = Pet.create(image: "image.jpeg", name: "Kunga", approximate_age: "1", sex: "male", shelter_id: shelter.id)
+    pet_2 = Pet.create(image: "image.jpeg", name: "Honey Pie", approximate_age: "11", sex: "female", shelter_id: shelter.id)
+
+    application = AdoptionApplication.create(name: "Stella", address: "street", city: "City", state: "ST", zip: "34567", phone_number: "545-567-7643", description: "I'm awesome")
+    application2 = AdoptionApplication.create(name: "Betsy", address: "street", city: "City", state: "ST", zip: "34567", phone_number: "545-567-7643", description: "I'm awesome as well")
+
+    PetApplication.create(pet_id: pet_1.id, adoption_application_id: application.id)
+    PetApplication.create(pet_id: pet_1.id, adoption_application_id: application2.id)
+
+    visit "/adoption_applications/#{application.id}"
+
+    within ".pets-#{pet_1.id}" do
+      click_link "Approve Application"
+    end
+
+    visit "/adoption_applications/#{application2.id}"
+
+    expect(page).to have_no_content("Approve Application")
+
+  end
 end
