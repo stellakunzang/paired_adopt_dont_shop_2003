0748823e2daff9ee9342694f4c5d539064da169a
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-19 12:40:08 -0600

    Adjust route for update method in pet applications controller

--- config/routes.rb
+++ config/routes.rb
@@ -32,4 +32,6 @@ Rails.application.routes.draw do
   get '/adoption_applications/new', to: 'adoption_application#new'
   post '/favorites', to: 'adoption_application#create'
   get '/adoption_applications/:application_id', to: 'adoption_application#show'
+  get '/pets/:pet_id/applications', to: 'pet_applications#index'
+  patch '/pets/:pet_id/applications/:application_id', to: 'pet_applications#update'
 end
