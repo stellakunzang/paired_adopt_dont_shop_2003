993959cec68b8ba8af55166e61c1c969cfc4e54b
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-18 19:02:33 -0600

    Add model test to confirm expected behavior in new class method

--- spec/models/adoption_application_spec.rb
+++ spec/models/adoption_application_spec.rb
@@ -17,5 +17,24 @@ RSpec.describe AdoptionApplication do
     it { should validate_presence_of :description}
   end
 
+  describe "class methods" do
+    it "#pets" do
+      shelter = Shelter.create(name: "Happy Puppies", address: "55 Street St", city: "Danger Mountain", state: "UT", zip: "80304")
+
+      pet_1 = Pet.create(image: "image.jpeg", name: "Kunga", approximate_age: "1", sex: "male", shelter_id: shelter.id)
+      pet_2 = Pet.create(image: "image.jpeg", name: "Honey Pie", approximate_age: "11", sex: "female", shelter_id: shelter.id)
+
+      application_1 = AdoptionApplication.create(name: "Stella", address: "street", city: "City", state: "ST", zip: "34567", phone_number: "545-567-7643", description: "I'm awesome")
+      application_2 = AdoptionApplication.create(name: "Stella", address: "street", city: "City", state: "ST", zip: "34567", phone_number: "545-567-7643", description: "I'm still awesome")
+
+      PetApplication.create(pet_id: pet_1.id, adoption_application_id: application_1.id)
+      PetApplication.create(pet_id: pet_2.id, adoption_application_id: application_1.id)
+      PetApplication.create(pet_id: pet_1.id, adoption_application_id: application_2.id)
+      PetApplication.create(pet_id: pet_2.id, adoption_application_id: application_2.id)
+
+      expect(AdoptionApplication.pets).to eq([pet_1, pet_2])
+    end
+  end
+
 
 end
