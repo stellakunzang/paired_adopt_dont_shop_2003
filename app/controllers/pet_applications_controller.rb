82587fdf9722301113f3686a6f686d77ca9eef93
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-19 15:47:32 -0600

    Add method to toggle approval status

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -7,8 +7,19 @@ class PetApplicationsController < ApplicationController
 
   def update
     pet = Pet.find(params[:pet_id])
-    app_id = params[:application_id]
     pet.update({adoption_status: "adoption pending"})
+    app_id = params[:application_id]
+    application = AdoptionApplication.find(app_id)
+    application.update({approval_status: toggle_approval_status(application)})
     redirect_to "/pets/#{pet.id}?application_id=#{app_id}"
   end
+
+  def toggle_approval_status(application)
+    if application.approval_status == "unapproved"
+      "approved"
+    else
+      "unapproved"
+    end
+  end
+
 end
