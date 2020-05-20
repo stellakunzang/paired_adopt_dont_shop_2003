8b48c7222c4a6d7e692fcc0f2cd0940550bba892
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-20 16:02:42 -0600

    Add test to shelter spec for applications on file

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe Shelter do
-  
+
   describe "relationships" do
     it {should have_many :pets}
     it {should have_many :shelter_reviews}
@@ -13,6 +13,25 @@ RSpec.describe Shelter do
     it { should validate_presence_of :city}
     it { should validate_presence_of :state}
     it { should validate_presence_of :zip}
+  end
+
+  describe "class_methods" do
+    it "#applications" do
+      shelter = Shelter.create(name: "Happy Puppies", address: "55 Street St", city: "Danger Mountain", state: "UT", zip: "80304")
+
+      pet_1 = Pet.create(image: "image.jpeg", name: "Kunga", approximate_age: "1", sex: "male", shelter_id: shelter.id)
+      pet_2 = Pet.create(image: "image.jpeg", name: "Honey Pie", approximate_age: "11", sex: "female", shelter_id: shelter.id)
+
+      application_1 = AdoptionApplication.create(name: "Stella", address: "street", city: "City", state: "ST", zip: "34567", phone_number: "545-567-7643", description: "I'm awesome")
+      application_2 = AdoptionApplication.create(name: "Stella", address: "street", city: "City", state: "ST", zip: "34567", phone_number: "545-567-7643", description: "I'm still awesome")
+
+      PetApplication.create(pet_id: pet_1.id, adoption_application_id: application_1.id)
+      PetApplication.create(pet_id: pet_2.id, adoption_application_id: application_1.id)
+      PetApplication.create(pet_id: pet_1.id, adoption_application_id: application_2.id)
+      PetApplication.create(pet_id: pet_2.id, adoption_application_id: application_2.id)
+
+      expect(shelter.applications).to eq(2)
+    end
   end
 
 end
