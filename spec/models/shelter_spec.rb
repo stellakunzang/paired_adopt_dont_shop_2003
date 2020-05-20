53e724df7ef9e8ca29daeb374827120e1fd9d655
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-20 16:59:50 -0600

    Add test for shelter model method counting approved applications

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -32,6 +32,18 @@ RSpec.describe Shelter do
 
       expect(shelter.applications).to eq(2)
     end
+
+    it "#approved_applicants" do
+      shelter = Shelter.create(name: "Happy Puppies", address: "55 Street St", city: "Danger Mountain", state: "UT", zip: "80304")
+
+      pet_1 = Pet.create(image: "image.jpeg", name: "Kunga", approximate_age: "1", sex: "male", shelter_id: shelter.id)
+
+      application_1 = AdoptionApplication.create(name: "Stella", address: "street", city: "City", state: "ST", zip: "34567", phone_number: "545-567-7643", description: "I'm awesome")
+
+      PetApplication.create(pet_id: pet_1.id, adoption_application_id: application_1.id, approval_status: "approved")
+
+      expect(shelter.approved_applicants).to eq(1)
+    end
   end
 
 end
