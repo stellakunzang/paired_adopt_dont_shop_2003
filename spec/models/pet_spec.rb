5707243d9343105eaf89942e475fa2b3214203d2
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-20 15:30:24 -0600

    Add test for approved applicant helper method in pet model

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -24,6 +24,18 @@ RSpec.describe Pet do
 
       expect(pet_1.adoption_status_text).to eq("Adoption Pending")
     end
+
+    it "#approved_applicant" do
+      shelter = Shelter.create(name: "Happy Puppies", address: "55 Street St", city: "Danger Mountain", state: "UT", zip: "80304")
+
+      pet_1 = Pet.create(image: "image.jpeg", name: "Kunga", approximate_age: "1", sex: "male", shelter_id: shelter.id)
+
+      application_1 = AdoptionApplication.create(name: "Stella", address: "street", city: "City", state: "ST", zip: "34567", phone_number: "545-567-7643", description: "I'm awesome")
+
+      PetApplication.create(pet_id: pet_1.id, adoption_application_id: application_1.id)
+
+      expect(pet_1.approved_applicant).to eq(nil)
+    end
   end
 
 end
