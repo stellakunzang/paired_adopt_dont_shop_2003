db29fc6bcfb80a77574b75cd26771d4646c10e85
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-19 18:11:49 -0600

    Move toggle status methods to application controller since used across controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,21 @@ class ApplicationController < ActionController::Base
   def favorite
     @favorite ||= Favorite.new(session[:favorite])
   end
+
+  def toggle_approval_status(pet_application)
+    if pet_application[:approval_status] == "unapproved"
+      "approved"
+    else
+      "unapproved"
+    end
+  end
+
+  def toggle_adoption_status(pet)
+    if pet.adoption_status == "adoptable"
+      "adoption pending"
+    else
+      "adoptable"
+    end
+  end
+
 end
