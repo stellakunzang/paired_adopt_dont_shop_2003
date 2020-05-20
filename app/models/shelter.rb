14fa2ab54f3aa414da3370356205d0c1ba54ae34
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-20 16:08:24 -0600

    Add method to model to count applications for shelter

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,5 +1,13 @@
 class Shelter < ApplicationRecord
   has_many :pets
-  has_many :shelter_reviews 
+  has_many :shelter_reviews
   validates_presence_of :name, :address, :city, :state, :zip
+
+  def applications
+    applications = []
+    pets.each do |pet|
+      applications << pet.adoption_applications
+    end
+    applications.flatten.uniq.count 
+  end
 end
